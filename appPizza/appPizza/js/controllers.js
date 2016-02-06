f4f79af08eb5e1ca162766a8fbc24eccaee20207
Fernando Moya <fernandoalexander2014@gmail.com>
2016-02-06 16:39:15 -0500

    Interfaz 1

---
+++ appPizza/appPizza/js/controllers.js
@@ -0,0 +1,38 @@
+angular.module('app.controllers', [])
+  
+.controller('loginCtrl', function($scope) {
+
+})
+   
+.controller('acercaDeCtrl', function($scope) {
+
+})
+      
+.controller('registroCtrl', function($scope) {
+
+})
+   
+.controller('creaTuPizzaCtrl', function($scope) {
+
+})
+   
+.controller('menúMamaMiaPizzaCtrl', function($scope) {
+
+})
+   
+.controller('pizzasCtrl', function($scope) {
+
+})
+   
+.controller('usuariosCtrl', function($scope) {
+
+})
+   
+.controller('descripcionCtrl', function($scope) {
+
+})
+   
+.controller('miOrdenCtrl', function($scope) {
+
+})
+ 
---
+++ appPizza/appPizza/js/routes.js
@@ -0,0 +1,129 @@
+angular.module('app.routes', [])
+
+.config(function($stateProvider, $urlRouterProvider) {
+
+  // Ionic uses AngularUI Router which uses the concept of states
+  // Learn more here: https://github.com/angular-ui/ui-router
+  // Set up the various states which the app can be in.
+  // Each state's controller can be found in controllers.js
+  $stateProvider
+    
+      
+        
+    .state('login', {
+      url: '/login',
+      templateUrl: 'templates/login.html',
+      controller: 'loginCtrl'
+    })
+        
+      
+    
+      
+        
+    .state('mamaMiaPizza.acercaDe', {
+      url: '/aboutUs',
+      views: {
+        'side-menu21': {
+          templateUrl: 'templates/acercaDe.html',
+          controller: 'acercaDeCtrl'
+        }
+      }
+    })
+        
+      
+    
+      
+    .state('mamaMiaPizza', {
+      url: '/sideMenuPizza',
+      abstract:true,
+      templateUrl: 'templates/mamaMiaPizza.html'
+    })
+      
+    
+      
+        
+    .state('registro', {
+      url: '/registryUsers',
+      templateUrl: 'templates/registro.html',
+      controller: 'registroCtrl'
+    })
+        
+      
+    
+      
+        
+    .state('mamaMiaPizza.creaTuPizza', {
+      url: '/createPizza',
+      views: {
+        'side-menu21': {
+          templateUrl: 'templates/creaTuPizza.html',
+          controller: 'creaTuPizzaCtrl'
+        }
+      }
+    })
+        
+      
+    
+      
+        
+    .state('mamaMiaPizza.menúMamaMiaPizza', {
+      url: '/menuPizza',
+      views: {
+        'side-menu21': {
+          templateUrl: 'templates/menúMamaMiaPizza.html',
+          controller: 'menúMamaMiaPizzaCtrl'
+        }
+      }
+    })
+        
+      
+    
+      
+        
+    .state('pizzas', {
+      url: '/descriptionPizza',
+      templateUrl: 'templates/pizzas.html',
+      controller: 'pizzasCtrl'
+    })
+        
+      
+    
+      
+        
+    .state('usuarios', {
+      url: '/users',
+      templateUrl: 'templates/usuarios.html',
+      controller: 'usuariosCtrl'
+    })
+        
+      
+    
+      
+        
+    .state('descripcion', {
+      url: '/descriptionUsers',
+      templateUrl: 'templates/descripcion.html',
+      controller: 'descripcionCtrl'
+    })
+        
+      
+    
+      
+        
+    .state('mamaMiaPizza.miOrden', {
+      url: '/myOrder',
+      views: {
+        'side-menu21': {
+          templateUrl: 'templates/miOrden.html',
+          controller: 'miOrdenCtrl'
+        }
+      }
+    })
+        
+      
+    ;
+
+  // if none of the above states are matched, use this as the fallback
+  $urlRouterProvider.otherwise('/login');
+
+});
